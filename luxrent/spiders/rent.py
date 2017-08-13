16d461874b21adfba7b9ffaf8f2b055c3cd331db
David K <kamkard@gmail.com>
2017-08-13 18:19:39 -0500

    Made list of files

--- luxrent/spiders/rent.py
+++ luxrent/spiders/rent.py
@@ -10,13 +10,29 @@ class RentSpider(scrapy.Spider):
 	start_urls = ['https://www.amli.com/apartments/austin/central-east-austin/austin/south-shore/floorplans']
 
 	def parse(self, response):
-"""
-java link to all rooms
-	 response.xpath('//div[@id="fpHolder"]/a/@href').extract()
-"""
-"""
-Find the apt room infos
-data = response.xpath('//tr[@class="highlightRowClicked" or @class = "highlightRow"]/td/*')
-data.xpath('.//span/text()').extract()
 
-"""
+		
+		#Find the apt room infos
+
+		for data in response.xpath('//tr[@class="highlightRowClicked" or @class = "highlightRow"]'):
+			yield {
+
+			'Floor' : data.xpath('.//span[contains(@id, "Floor")]/text()').extract(),
+			'RoomNumber' : data.xpath('.//span[contains(@id, "Number")]/text()').extract(),
+			'Pets' : data.xpath('.//span[contains(@id, "Pets")]/text()').extract(),
+			'Dates' : data.xpath('.//span[contains(@id, "UnitDates")]/text()').extract(),
+			'Price' : data.xpath('.//span[contains(@id, "Price")]/text()').extract(),
+
+			}
+
+			#print(dict(floor=floor, RoomNumber=RoomNumber, Pets=Pets, Dates=Dates, Price=Price))
+
+		#java link to all rooms
+		
+		response.xpath('//div[@id="fpHolder"]/a/@href').extract()
+
+
+
+		
+		
+
